64a210d2c3c5b026ecd81e87296257e4a8d22596
joaozinhoDoGrauxD <joaoninguem1301@gmail.com>
2026-02-11 23:08:02 -0300

    feat: config directory and simple connection

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 import express from 'express';
 import 'dotenv/config';
-import sequelize from "./src/models/database.js"
+import sequelize from "./src/config/sequelize.js";
+import redis from "./src/config/redis.js"
+import {uri, mongoose} from "./src/config/mongodb.js"
 const app = express();
+
 
 try {
   await sequelize.authenticate();
@@ -9,6 +12,20 @@ try {
 } catch (error) {
   console.error('Unable to connect to the database:', error);
 }
+
+redis.on('error', err => console.log('Redis Client Error', err));
+
+await redis.connect()
+
+mongoose.connect(uri, {
+    serverSelectionTimeoutMS: 5000 
+})
+.then(() => console.log('Banco Conectado com SUCESSO!'))
+.catch(err => {
+    console.error(' ERRO CRÍTICO DE CONEXÃO:');
+    console.error('Motivo:', err.message); 
+    console.error('Verifique: 1. Senha na string; 2. IP no Network Access (0.0.0.0/0)');
+});
 
 export default app;
 
---
+++ src/config/redis.js
@@ -0,0 +1,11 @@
+import { createClient} from 'redis';
+const port = process.env.PORT_REDIS;
+const host = process.env.HOST_REDIS;
+const user = process.env.USERNAME_REDIS;
+const password = process.env.PASSWORD_REDIS;
+
+const redis = await createClient({
+    url : `redis://${user}:${password}@${host}:${port}` 
+});
+
+export default redis;
